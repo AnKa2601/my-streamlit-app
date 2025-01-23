da3db603b1cb711eebe572b44cf657adbc6cc8f8
AnKa2601 <kaushikanuja15@gmail.com>
2025-01-23 17:28:26 +0530

    Update my-streamlit-app.py

--- my-streamlit-app.py
+++ my-streamlit-app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from audiorecorder import audiorecorder
 
 st.title('SpeechBrain - Speech to Text')
 st.write('Hello world')
+audio = audiorecorder("Click to record", "Click to stop recording")
+
+if len(audio) > 0:
+
+    
+    st.audio(audio.export().read()) 
+    
